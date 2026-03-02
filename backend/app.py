0dcb1c44aa645f69d6a6f815afe2c44ec4febd83
Diego Rivas <drivas022@yahoo.com>
2026-03-02 15:54:57 -0600

    feat: add ORM model for table

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,40 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request # flask crea la aplicación web, jsonify convierte diccionarios/listas d epyhton en JSON y request deja leer lo que nos manda el cliente (JSON del POST)
+from flask_cors import CORS # permite que http://localhost:3000 (React) pueda llamar a http://localhost:5000 (Flask). # Importa CORS para permitir que React (otro puerto) pueda llamar a este backend sin bloqueo
+from flask_sqlalchemy import SQLAlchemy # es le ORM, permite crear modelos (clases) que representan tablas y hacer queries sin SQL manual
+
 
 # llamando dependencias e inicializando la app
 app = Flask(__file__)
+CORS(app) # Esto permite llamadas desde http://localhost:3000 hacia http://localhost:5000.
 
-# decorador
-@app.route('/')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqllite:///songs.db" # URI de la base de datos, sqllite es un motor de base de datos liviano que guarda todo en un archivo local llamado songs.db
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # Desactiva el seguimiento de modificaciones para mejorar el rendimiento
 
-def home():
-    return jsonify({'message': 'Hello, World!'})
+db = SQLAlchemy(app) # Inicializa SQLAlchemy con la aplicación Flask
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Modelo ORM: Esta clase representa una tabla en la BD
+class Song(db.Model):
+    # nombre real de la tabla en la bd
+    __tablename__ = "songs"
+
+    # columnas de la tabla
+    idSong = db.Column(db.Integer, primary_key=True) 
+    songName = db.Column(db.String(250), nullable=False)
+    artist =db.Column(db.String(250), nullable=False)
+
+    # metodo para convertir un objeto song a diccionario (para devolverlo como JSON)
+    def to_dict(self):
+        return {
+            "idSong": self.idSong,
+            "songName": self.songName,
+            "artist": self.artist
+        }
+
+# Este bloque solo se ejecuta si corres el archivo directamente: python app.py
+if __name__ == "__main__":
+    # necesitamos un contexto de la app para que SQLAlchemy pueda crear las tablas
+    with app.app_context():
+        db.create_all() # Crea las tablas si no existen (songs.db + tabla songs)
+
+    # corre el servidor en el puerto 5000, debug=True reinicia automáticamente cuando guardas cambios (para desarrollo)
+    app.run(host="0.0.0.0", port=5000, debug=True)
